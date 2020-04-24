cf30c6af383ae1b22dafcac490573ae9091df9b1
nikowis <nikowis@gmail.com>
2020-04-24 20:03:12 +0200

    Test fix

--- src/test/java/pl/nikowis/ksiazkofilia/TestConstants.java
+++ src/test/java/pl/nikowis/ksiazkofilia/TestConstants.java
@@ -2,6 +2,6 @@ package pl.nikowis.ksiazkofilia;
 
 public class TestConstants {
 
-    public final static String LOGIN = "testuser2@email.com";
+    public final static String LOGIN = "testuser@email.com";
     public final static String LOGIN2 = "testuser2@email.com";
 }
