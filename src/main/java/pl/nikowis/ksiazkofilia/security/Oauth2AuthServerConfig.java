09a23d00b4f11b55a37076280ff1a04085f39d7e
nikowis <nikowis@gmail.com>
2020-05-06 21:23:22 +0200

    Small oauth improvments

--- src/main/java/pl/nikowis/ksiazkofilia/security/Oauth2AuthServerConfig.java
+++ src/main/java/pl/nikowis/ksiazkofilia/security/Oauth2AuthServerConfig.java
@@ -1,9 +1,11 @@
 package pl.nikowis.ksiazkofilia.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -18,11 +20,20 @@ import javax.sql.DataSource;
 @EnableAuthorizationServer
 public class Oauth2AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    public static final String WEB_CLIENT_ID = "webClient";
+    public static final String WEB_CLIENT_SECRET = "webClientSecret";
+
     @Autowired
     private DataSource dataSource;
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Value("${oauth.token.validity.seconds}")
+    private int tokenValiditySeconds;
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
@@ -35,11 +46,11 @@ public class Oauth2AuthServerConfig extends AuthorizationServerConfigurerAdapter
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .inMemory()
-                .withClient("frontendClientId")
-                .secret("$2y$10$Rit16FIsp5QIf5vfV2FzVeGLw28BygNOAls3qaJyCCylA8LS30FkG")
+                .withClient(WEB_CLIENT_ID)
+                .secret(encoder.encode(WEB_CLIENT_SECRET))
                 .authorizedGrantTypes("password","authorization_code", "refresh_token")
-                .accessTokenValiditySeconds(3600)
-                .refreshTokenValiditySeconds(28*24*3600)
+                .accessTokenValiditySeconds(tokenValiditySeconds)
+                .refreshTokenValiditySeconds(tokenValiditySeconds)
                 .scopes("read");
     }
 
