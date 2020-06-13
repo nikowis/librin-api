0fd7932e770d466a92c6bf7f3fa6ba17e18655b1
nikowis <nikowis@gmail.com>
2020-06-13 15:05:48 +0200

    Configure simple unauthorized websocket connection

---
+++ src/main/java/pl/nikowis/librin/websocket/GreetingController.java
@@ -0,0 +1,19 @@
+package pl.nikowis.librin.websocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/users/1/greeting")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
