a33971d53458e05d6023c6bb6da5f28c7f90680f
nikowis <nikowis@gmail.com>
2020-11-22 23:23:27 +0100

    Fix name and password regex

--- src/main/java/pl/nikowis/librin/infrastructure/security/SecurityConstants.java
+++ src/main/java/pl/nikowis/librin/infrastructure/security/SecurityConstants.java
@@ -2,12 +2,12 @@ package pl.nikowis.librin.infrastructure.security;
 
 public class SecurityConstants {
 
-    public final static String LETTERS_GROUP_WITH_POLISH_CHARS = "[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]";
+    public final static String LETTERS_GROUP_WITH_POLISH_CHARS = "[a-zA-Z\u0104\u0106\u0118\u0141\u0143\u015A\u0179\u017B\u0105\u0107\u0119\u0142\u0144\u015B\u017A\u017C\u00F3\u00D3-]";
     public static final String NAME_REGEX = "^[" + LETTERS_GROUP_WITH_POLISH_CHARS + "]+$";
     /*
         Password that contains 8 characters, no white spaces, at least one digit or special character
      */
-    public final static String PSWD_REGEX = "^(?=.*[" + LETTERS_GROUP_WITH_POLISH_CHARS + "])(?=.*[0-9])(?=\\S+$).{8,}$";
+    public final static String PSWD_REGEX = "^.{8,}$";
     public static final String ROLE_USER = "USER";
     public static final String ROLE_ADMIN = "ADMIN";
     public static final String ROLE_MOD = "MOD";
