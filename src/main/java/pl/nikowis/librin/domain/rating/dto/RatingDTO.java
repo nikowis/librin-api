0a6edc3d7296367cbe206f88532e95526a325903
nikowis <nikowis@gmail.com>
2020-11-26 21:58:07 +0100

    Added date to ratings

--- src/main/java/pl/nikowis/librin/domain/rating/dto/RatingDTO.java
+++ src/main/java/pl/nikowis/librin/domain/rating/dto/RatingDTO.java
@@ -3,10 +3,13 @@ package pl.nikowis.librin.domain.rating.dto;
 import lombok.Data;
 import pl.nikowis.librin.domain.user.dto.PublicUserDTO;
 
+import java.util.Date;
+
 @Data
 public class RatingDTO {
 
    private Short value;
    private PublicUserDTO author;
    private String description;
+   private Date createdAt;
 }
